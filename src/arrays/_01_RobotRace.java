07e2d3860c3b49a1a5579204f292499ef75a4198
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-12-11 16:57:11 -0800

    Robot Race

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 
@@ -8,9 +10,47 @@ public class _01_RobotRace {
  public static void main(String[] args) {
 	 Robot[] rob = new Robot[5];
 	 for (int i = 0; i < 4 ; i++) {
-		 rob[i] = new Robot();
+		 rob[i] = new Robot("june");
 		 rob[i].moveTo(100*i+ 100, 500);
+		 
 	 }
+	
+	 for (int w = 0; w < 2; w++) {
+		
+	
+		
+		 Random rand = new Random();
+		 int rand1 = rand.nextInt(500);
+		 rob[0].move(rand1); 
+		  rand1 = rand.nextInt(500);
+		 rob[1].move(rand1); 
+		  rand1 = rand.nextInt(500);
+		 rob[2].move(rand1); 
+		  rand1 = rand.nextInt(500);
+		 rob[3].move(rand1); 
+		  rand1 = rand.nextInt(500);
+		 rob[4].move(rand1); 
+		 rob[0].setSpeed(100);
+		 rob[1].setSpeed(100);
+		 rob[2].setSpeed(100);
+		 rob[3].setSpeed(100);
+		 rob[4].setSpeed(100);
+		int rob1x = rob[0].getX();
+		int rob2x = rob[1].getX();
+		int rob3x = rob[2].getX();
+		int rob4x =	 rob[3].getX();
+		int rob5x =	 rob[4].getX();
+		 
+		 
+		 if (rob1x > 0 || rob2x > 0 || rob3x > 0 || rob4x > 0 || rob5x > 0) {
+			 w = 3;
+		 }
+		 else {
+			 w = 1;
+		 }
+		 
+	 }
+
 } 
 	//2. create an array of 5 robots.
  
